7761f8ef7b3bf0efe478461eefbcec685ad17bb9
faramarz kord <frytiger54@gmail.com>
2022-10-02 00:40:51 +0330

    upload files

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const rangeInput = document.querySelector('input')
+const container = document.querySelector('.container')
+
+
+rangeInput.addEventListener('change', function (event) {
+    container.style.filter = 'brightness(' + event.target.value + '%)'
+})
